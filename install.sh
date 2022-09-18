8ae75fa925eeb92d09249d85ca35c361edcff3f0
Gregoire <gchalony@mailfence.com>
2022-09-18 13:20:19 +0200

    Nemo implementation

---
+++ install.sh
@@ -0,0 +1,5 @@
+cp store_paths.nemo_action ~/.local/share/nemo/actions
+cp store_paths ~/.local/bin
+cp link_stored.nemo_action ~/.local/share/nemo/actions
+cp link_stored ~/.local/bin
+echo Done 🎖 
